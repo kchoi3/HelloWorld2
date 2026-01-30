6d2a9b3a1094a2ace0aff4f2b22a82054ce65128
Kwang Seo Choi <kchoi3@umd.edu>
2026-01-30 18:34:42 -0500

    Initial commit of main application file Kwang Seo Choi kchoi3@terpmail.umd.edu

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World from Kwang Seo Choi'
+
+
+if __name__ == '__main__':
+    app.run()
