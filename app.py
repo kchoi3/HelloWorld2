8be40a24f233ff1ed5a9220a362c4e213224c9b5
alexchoi <kchoi3@terpmail.umd.edu>
2026-02-06 14:56:48 -0500

    Added code to demonstrate a first code change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Kwang Seo Choi'
+    return 'Hello World from Kwang Seo Choi! I am adding my first code change'
 
 
 if __name__ == '__main__':
