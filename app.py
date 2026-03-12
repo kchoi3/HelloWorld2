4ce25602e8ba8c553b850234f736b3140a54bd60
alexchoi <kchoi3@terpmail.umd.edu>
2026-03-11 20:32:36 -0400

    Created new routes for favorite course and contact. Created parent template on base.html. Incorporated child templates on contact and favorite course.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -18,6 +18,22 @@ def about():
 def aboutcss():
     return render_template('about-css.html')
 
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    print('Subject entered: ' + subject)
+    print('Course Number entered: ' + course_number)
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html', form_submitted=False)
+    return render_template('contact.html', form_submitted=False)
 
 if __name__ == '__main__':
     app.run()
